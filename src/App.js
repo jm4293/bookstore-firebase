e3e2dde2d514cf619b273deb50d346241d42f017
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-24 19:43:41 +0900

    feat: 상품 클릭시 상품 상세내용 페이지로 이동

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 // import { useDispatch, useSelector } from 'react-redux';
 import Home from './components/Home/Home';
 import SignUp from './components/SignUp/SignUp';
@@ -7,33 +7,34 @@ import Agree from './components/SignUp/Agree';
 import Register from './components/SignUp/Register';
 import SignIn from './components/SignIn/SignIn';
 import Admin from './components/Admin/Admin';
-import { Routes, Route } from "react-router-dom";
+import Detail from "./components/Detail";
+import {Routes, Route} from "react-router-dom";
 import app from './Firebase/firebase';
 
 function App() {
-  // const user = useSelector(state => state.userSlice)
-  // const dispatch = useDispatch();
+    // const user = useSelector(state => state.userSlice)
+    // const dispatch = useDispatch();
 
-  return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/signup/agree" element={<Agree/>}/>
-        <Route path="/signup/register" element={<Register/>}/>
-        <Route path="/signin" element={<SignIn/>}/>
-        <Route path="/admin" element={<Admin/>}/>
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <GlobalStyle/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/signup" element={<SignUp/>}/>
+                <Route path="/signup/agree" element={<Agree/>}/>
+                <Route path="/signup/register" element={<Register/>}/>
+                <Route path="/signin" element={<SignIn/>}/>
+                <Route path="/admin" element={<Admin/>}/>
+                <Route path="/detail/:code" element={<Detail/>}/>
+            </Routes>
+        </>
+    );
 }
 
 const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    /* width: 100vw; */
   }
 `;
 
---
+++ src/components/Detail.jsx
@@ -0,0 +1,111 @@
+import React, {useEffect, useState} from "react";
+import styled from "styled-components";
+import {useParams} from "react-router-dom";
+import {db, storage} from "../Firebase/firebase";
+import {collection, query, where, getDocs} from "firebase/firestore";
+import {ref, getDownloadURL} from "firebase/storage";
+
+
+function Detail() {
+    let {code} = useParams()
+    const [item, setItem] = useState({});
+    const [img, setImg] = useState("");
+    console.log(code)
+
+    useEffect(() => {
+        const q = query(collection(db, "books"), where("code", "==", code));
+
+        async function querySnapshot() {
+            return await getDocs(q);
+        }
+        querySnapshot()
+            .then((data) => {
+                data.forEach(element => setItem(element.data()))
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+        getDownloadURL(ref(storage, `${code}`))
+            .then((url) => {
+                setImg(url);
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
+    return (
+        <Frame>
+            <Header>
+                <img src="/images/logo.jpeg" onClick={() => window.location.replace('/')} style={{height: "100%", cursor: "pointer"}}/>
+            </Header>
+            <Footer>
+                <Left>
+                    <h3>{`저자: ${item.author}`}</h3>
+                    <h3>{`종류: ${item.kind}`}</h3>
+                </Left>
+                <Middle>
+                    <h1>{`${item.title}`}</h1>
+                    {/*<div>{item.title}</div>*/}
+                    <img src={img}/>
+                </Middle>
+                <Right>
+                    <h2>{`가격: ${item.price}원`}</h2>
+                    <h4>{`출판일: ${item.release}`}</h4>
+                    <h4>{`코드: ${item.code}`}</h4>
+                </Right>
+
+            </Footer>
+        </Frame>
+    )
+}
+
+const Frame = styled.div`
+  width: 90vw;
+  height: 100vh;
+  margin: auto;
+  //border: 1px solid black;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 10%;
+  border-bottom: 1px solid black;
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  height: 90%;
+  display: flex;
+`;
+
+const Left = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  //border: 1px solid red;
+`;
+
+const Middle = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-left: 1px solid black;
+  border-right: 1px solid black;
+`;
+
+const Right = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  //border: 1px solid blue;
+`;
+
+export default Detail;
